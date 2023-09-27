7185360603d0e2b72f55dc20699c3f7f809fab9e
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-09-27 17:40:08 +0200

    feat(flake.nix): add initial version

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "NAHO's NixOS Flake";
+
+  inputs = {
+    flakeUtils.url = "github:numtide/flake-utils";
+    impermanence.url = "github:nix-community/impermanence";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    preCommitHooks = {
+      inputs = {
+        flake-utils.follows = "flakeUtils";
+        nixpkgs-stable.follows = "preCommitHooks/nixpkgs";
+        nixpkgs.follows = "nixpkgs";
+      };
+
+      url = "github:cachix/pre-commit-hooks.nix";
+    };
+  };
+
+  outputs = {
+    self,
+    flakeUtils,
+    nixpkgs,
+    preCommitHooks,
+    ...
+  } @ inputs:
+    flakeUtils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        checks = {
+          nixosConfigurationsMasterplan =
+            self.nixosConfigurations.masterplan.config.system.build.toplevel;
+
+          preCommitHooks = preCommitHooks.lib.${system}.run {
+            hooks = {
+              alejandra.enable = true;
+              convco.enable = true;
+            };
+
+            settings.alejandra.verbosity = "quiet";
+            src = ./.;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          inherit (self.checks.${system}.preCommitHooks) shellHook;
+        };
+      }
+    )
+    // {
+      nixosConfigurations = import ./hosts {inherit inputs;};
+    };
+}
