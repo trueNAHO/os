a179f002cfc1c5dab8eec9f8056e75092a0b1a92
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-09-27 17:38:51 +0200

    feat(hosts/masterplan): add initial version

---
+++ hosts/masterplan/default.nix
@@ -0,0 +1,64 @@
+{inputs, ...}: {
+  masterplan = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      {
+        imports = [
+          "${inputs.impermanence}/nixos.nix"
+          ../../modules/networking.nix
+          ../../modules/nix/optimisation.nix
+          ../../modules/nix/settings/flake.nix
+          ../../modules/services/xserver/desktopManager.nix
+          ../../modules/services/xserver/displayManager.nix
+          ../../modules/sound.nix
+          ../../modules/users/users/naho.nix
+          ./hardware-configuration.nix
+        ];
+
+        modules = {
+          networking = {
+            enable = true;
+            hostName = "masterplan";
+          };
+
+          nix = {
+            flake.enable = true;
+            optimisation.enable = true;
+          };
+
+          services.xserver = {
+            desktopManager.enable = true;
+            displayManager.enable = true;
+          };
+
+          sound.enable = true;
+
+          users.users.naho = {
+            enable = true;
+            insecureInitialHashedPassword = true;
+          };
+        };
+
+        boot.loader = {
+          efi.canTouchEfiVariables = true;
+
+          systemd-boot = {
+            enable = true;
+            editor = false;
+          };
+        };
+
+        environment = {
+          etc."machine-id".text = "c9afd40dc75e45c593c2fe07274e4395";
+          persistence."/persistent".directories = ["/etc/ssh"];
+        };
+
+        fileSystems."/persistent".neededForBoot = true;
+        nixpkgs.config.allowUnfree = true;
+        programs.dconf.enable = true;
+        services.printing.enable = true;
+        system.stateVersion = "23.05";
+        time.timeZone = "Europe/Luxembourg";
+      }
+    ];
+  };
+}
