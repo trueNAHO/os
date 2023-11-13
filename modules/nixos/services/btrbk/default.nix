104a15dbaddee2c0b2467e4326522cba94d8d8ea
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-11-13 23:36:39 +0100

    feat(modules/nixos/services/btrbk): increase frequency

--- modules/nixos/services/btrbk/default.nix
+++ modules/nixos/services/btrbk/default.nix
@@ -6,11 +6,15 @@
   options.modules.nixos.services.btrbk.enable = lib.mkEnableOption "btrbk";
 
   config = lib.mkIf config.modules.nixos.services.btrbk.enable {
-    services.btrbk.instances.home.settings = {
-      snapshot_dir = "/nix/btrbk";
-      snapshot_preserve = "1w";
-      snapshot_preserve_min = "1d";
-      subvolume = "/home";
+    services.btrbk.instances.home = {
+      onCalendar = "hourly";
+
+      settings = {
+        snapshot_dir = "/nix/btrbk";
+        snapshot_preserve = "1w";
+        snapshot_preserve_min = "1d";
+        subvolume = "/home";
+      };
     };
   };
 }
