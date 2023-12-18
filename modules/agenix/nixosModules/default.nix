d166d0a01ec162679bd6c0665eeeca71418cb4ca
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-12-18 19:50:48 +0100

    refactor(modules/agenix/nixosModules): remove irrelevant configuration

--- modules/agenix/nixosModules/default.nix
+++ modules/agenix/nixosModules/default.nix
@@ -1,10 +1,8 @@
 {
-  config,
   inputs,
   lib,
   ...
 }: {
   imports = [inputs.agenix.nixosModules.default];
   options.modules.agenix.nixosModules.enable = lib.mkEnableOption "agenix";
-  config = lib.mkIf config.modules.agenix.nixosModules.enable {};
 }
