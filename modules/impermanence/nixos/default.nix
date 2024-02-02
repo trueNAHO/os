68be88920e1f9dc8faac1d2efe086a7ce1e71e1c
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-02-02 15:54:16 +0100

    refactor(modules/impermanence/nixos): add 'btrfsSnapshots' options

--- modules/impermanence/nixos/default.nix
+++ modules/impermanence/nixos/default.nix
@@ -10,6 +10,22 @@
   ];
 
   options.modules.impermanence.nixos = {
+    btrfsSubvolumes = {
+      enable = lib.mkEnableOption "Btrfs subvolume darling erasure";
+
+      rootFilesystem = lib.mkOption {
+        description = "Path to the Btrfs root filesystem.";
+        example = "/dev/mapper/luks";
+        type = lib.types.str;
+      };
+
+      rootSubvolume = lib.mkOption {
+        description = "Name of the Btrfs root subvolume.";
+        example = "root";
+        type = lib.types.str;
+      };
+    };
+
     enable = lib.mkEnableOption "impermanence";
 
     path = lib.mkOption {
@@ -24,6 +40,41 @@
   in
     lib.mkIf cfg.enable {
       modules.agenix.nixosModules.default.enable = true;
+
+      # Reference:
+      #
+      # - https://github.com/nix-community/impermanence/blob/cd13c2917eaa68e4c49fea0ff9cada45440d7045/README.org?plain=1#L91-L157
+      boot.initrd.postDeviceCommands =
+        lib.mkIf
+        cfg.btrfsSubvolumes.enable
+        (lib.mkAfter ''
+          (
+            set -e
+
+            btrfs_subvolume_delete_recursive() {
+              btrfs subvolume list -o "$1" |
+                awk '{ print $NF }' |
+                while read -r subvolume; do
+                  btrfs_subvolume_delete_recursive "$mount_point/$subvolume"
+                done
+
+              btrfs subvolume delete "$1"
+            }
+
+            mount_point="$(mktemp --directory)"
+            mount "${cfg.btrfsSubvolumes.rootFilesystem}" "$mount_point"
+
+            trap 'umount "$mount_point" && rmdir "$mount_point"' EXIT
+
+            btrfs_subvolume_delete_recursive \
+              "$mount_point/${cfg.btrfsSubvolumes.rootSubvolume}"
+
+            btrfs \
+              subvolume \
+              create \
+              "$mount_point/${cfg.btrfsSubvolumes.rootSubvolume}"
+          )
+        '');
 
       environment.persistence.${cfg.path}.directories = [
         "/etc/ssh"
